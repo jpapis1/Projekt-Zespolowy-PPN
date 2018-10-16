02b968d61fda0a1c038ffb2cbcd3298d6a115388
jpapis1 <jpapis1@gmail.com>
2018-10-16 13:54:20 +0200

    Test Version 0.1

---
+++ DesktopApp/TradeAndStocks/src/main/java/app/Application.java
@@ -0,0 +1,49 @@
+/*
+ *    Copyright 2018-2019 Jacek Papis, Michał Piątek, Zofia Napierała
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ *
+ *    Data provided for free by IEX https://iextrading.com/developer/.
+ *    View IEX’s Terms of Use https://iextrading.com/api-exhibit-a/.
+ */
+
+package app;
+
+import app.model.User;
+import app.repository.UserRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class Application {
+    private static final Logger log = LoggerFactory.getLogger(Application.class);
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class);
+
+    }
+
+    @Bean
+    public CommandLineRunner run(UserRepository repository) {
+        return (args) -> {
+            repository.save(new User("test@test.com", "jarek","haslo123","Jerzy","Hop"));
+            repository.save(new User("bogus@test.com", "boguś","paSSWORD2","Bogusław","Linda"));
+        };
+    }
+
+
+}
---
+++ DesktopApp/TradeAndStocks/src/main/java/app/model/User.java
@@ -0,0 +1,96 @@
+/*
+ *    Copyright 2018-2019 Jacek Papis, Michał Piątek, Zofia Napierała
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ *
+ *    Data provided for free by IEX https://iextrading.com/developer/.
+ *    View IEX’s Terms of Use https://iextrading.com/api-exhibit-a/.
+ */
+
+package app.model;
+
+import app.repository.UserRepository;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class User {
+
+    @Id
+    @GeneratedValue(strategy=GenerationType.AUTO)
+    private Integer id;
+    private String email;
+    private String username;
+    private String password;
+    private String name;
+    private String surname;
+
+    public User(String email, String username, String password, String name, String surname) {
+        this.email = email;
+        this.username = username;
+        this.password = password;
+        this.name = name;
+        this.surname = surname;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+}
