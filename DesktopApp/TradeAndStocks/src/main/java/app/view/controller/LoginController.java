c9084b8f496cacce9dcdaa355099998d997f9db0
Sevitte <sevitte@o2.pl>
2018-12-19 15:19:10 +0100

    progress bar added, not working yet

--- DesktopApp/TradeAndStocks/src/main/java/app/view/controller/LoginController.java
+++ DesktopApp/TradeAndStocks/src/main/java/app/view/controller/LoginController.java
@@ -7,6 +7,7 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.ProgressBar;
 import javafx.scene.control.TextField;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
@@ -26,13 +27,16 @@ public class LoginController {
     TextField passwordField;
     @FXML
     private Text actionTarget;
-
+    @FXML
+    private ProgressBar progressBar;
     @FXML
     protected void handleSubmitButtonAction(ActionEvent event) throws IOException {
+        progressBar.setProgress(0);
         System.out.println(userService);
         actionTarget.setText("You are logging in");
         if (userService.isPasswordCorrect(loginOrPasswordField.getText(), passwordField.getText())) {
             Parent loginParent = FXMLLoader.load(getClass().getResource("/fxml/window/menu.fxml"));
+            progressBar.setProgress(100);
             Scene menu = new Scene(loginParent);
             Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
             stage.setScene(menu);
