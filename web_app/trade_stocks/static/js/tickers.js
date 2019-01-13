cc25541541cd7f00b8effc18937445b9a6dd1416
Haruspex <mpiatek010@gmail.com>
2019-01-13 13:02:32 +0100

    Tickers menu working

---
+++ web_app/trade_stocks/static/js/tickers.js
@@ -0,0 +1,45 @@
+
+
+var app3 = new Vue({
+    delimiters: ['[[', ']]'],
+    el: '#app-3',
+    data: {
+        seen: true,
+        message: jsonObject[0],
+        beg: 0,
+        end: 20,
+        range: [0,1,2,3,4,5,6,7,8,9,10,11,12,13
+                ,14,15,16,17,18,19,20]
+    },
+    methods: {
+    getData: function (i) {
+      
+      this.message = jsonObject[i]
+    },
+
+    addRange: function() {
+        if( this.end <= 8725-20){
+            this.beg += 20
+            this.end += 20
+            var list = [];
+            for (var i = this.beg; i <= this.end; i++) {
+                list.push(i);
+            }
+            this.range = list
+        }
+
+    },
+    removeRange: function() {
+        if(this.beg != 0){
+            this.beg -= 20
+            this.end -= 20
+            var list = [];
+            for (var i = this.beg; i <= this.end; i++) {
+                list.push(i);
+            }
+            this.range = list
+        }
+
+    }
+  }
+})
